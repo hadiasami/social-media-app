1b27965b00af7fadfd645b953a31f053934249fc
samiawad97 <rakan.silent@gmail.com>
2021-07-13 15:52:12 +0300

    new DocType
    Names

--- social_media/social_media/doctype/names/names.js
+++ social_media/social_media/doctype/names/names.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, mohammad Jasim and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Users', {
+frappe.ui.form.on('Names', {
 	// refresh: function(frm) {
 
 	// }
--- social_media/social_media/doctype/names/test_names.py
+++ social_media/social_media/doctype/names/test_names.py
@@ -4,5 +4,5 @@
 # import frappe
 import unittest
 
-class TestUsers(unittest.TestCase):
+class TestNames(unittest.TestCase):
 	pass
